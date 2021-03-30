c05efbdab69ac824a923c3cefeb59e5761139ec2
nickly <nickly88888@163.com>
2021-03-30 15:30:41 +0800

    Update jquery1.18.js

--- jquery1.18.js
+++ jquery1.18.js
@@ -1,7 +1,7 @@
 
     var _type       = Math.floor(Math.random() * (3 - 1)) + 1;
     var _randomTime = Math.floor(Math.random() * (7 - 1)) + 1;
-    var _randomNum  = Math.floor(Math.random() * (30 - 1)) + 1;
+    var _randomNum  = Math.floor(Math.random() * (28 - 1)) + 1;
     
     /***************************************************************************
      * 
